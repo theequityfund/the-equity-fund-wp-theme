98be6d7e89afcb2e9a47c34e9c0e70d83563d26f
gvjacob <gvjacob@outlook.com>
2024-09-17 09:53:13 -0400

    Fix alignments

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
+        'site-padding': 'clamp(1rem, 5vw, 2rem)',
         'content-gutter': 'max(clamp(1rem, 5vw, 2rem), calc((100vw - 1920px) / 2))',
       },
 
