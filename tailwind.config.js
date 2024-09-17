a26279fb5bd485e4532dc55e068e8b2ad4cb633b
gvjacob <gvjacob@outlook.com>
2024-09-17 09:42:18 -0400

    Trim down gutenberg classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./themes/the-equity-fund/**/*.twig', './themes/the-equity-fund/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'content-gutter': 'max(clamp(1rem, 5vw, 2rem), calc((100vw - 1920px) / 2))',
+      },
+
+      maxWidth: {
+        article: '800px',
+        'article-wide': '1200px',
+        '1/3': '33.333333%',
+      },
+    },
   },
   plugins: [],
 };
