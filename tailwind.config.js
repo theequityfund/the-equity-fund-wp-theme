01de9bdbb78aa97c7f58d330c9ecb26c59a20a7f
gvjacob <gvjacob@outlook.com>
2024-09-19 17:24:16 -0400

    Load fonts and configure in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./themes/the-equity-fund/**/*.twig', './themes/the-equity-fund/**/*.js'],
   theme: {
+    fontFamily: {
+      display: ['Judge', 'sans-serif'],
+      sans: ['Inter', 'sans-serif'],
+      serif: ['Feature Deck', 'serif'],
+      'serif-text': ['Feature Text', 'serif'],
+    },
+
     extend: {
       spacing: {
         'site-padding': 'clamp(1rem, 5vw, 2rem)',
