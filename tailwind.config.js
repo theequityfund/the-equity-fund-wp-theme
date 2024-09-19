55eb5647905b34525aee79aaef787cc3d2c20fd0
gvjacob <gvjacob@outlook.com>
2024-09-19 17:28:09 -0400

    Add base colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,20 @@ module.exports = {
       'serif-text': ['Feature Text', 'serif'],
     },
 
+    colors: {
+      white: '#FFFFFF',
+      black: '#111111',
+      maroon: '#500C20',
+      lavender: '#C4B3E0',
+      teal: '#18C198',
+      yellow: '#FFD64A',
+      cream: '#FCF6ED',
+
+      transparent: 'transparent',
+      current: 'currentColor',
+      inherit: 'inherit',
+    },
+
     extend: {
       spacing: {
         'site-padding': 'clamp(1rem, 5vw, 2rem)',
