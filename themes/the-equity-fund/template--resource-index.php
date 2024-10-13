378cf49a94378b23c5bb5ae41d15ea31dfc21432
gvjacob <gvjacob@outlook.com>
2024-10-13 15:49:41 -0400

    Query by intervention

--- themes/the-equity-fund/template--resource-index.php
+++ themes/the-equity-fund/template--resource-index.php
@@ -19,25 +19,51 @@ if ( post_password_required( $_page->ID ) ) {
 	Timber::render( 'pages/password.twig', $context );
 } else {
 	global $paged;
-	$query = isset( $_GET['query'] ) ? sanitize_text_field( $_GET['query'] ) : '';
+	$query         = isset( $_GET['query'] ) ? sanitize_text_field( $_GET['query'] ) : '';
+	$interventions = isset( $_GET['interventions'] ) ? $_GET['interventions'] : array();
+	$interventions = array_map( 'sanitize_text_field', $interventions );
 
 	$context['page'] = $_page;
 
-	$context['resources'] = Timber::get_posts(
-		array(
-			'post_type'      => 'resource',
-			'posts_per_page' => 4, // TODO: Update to 6.
-			'paged'          => $paged,
-			's'              => $query,
-		)
-	);
+	if ( empty( $interventions ) ) {
+		$context['resources'] = Timber::get_posts(
+			array(
+				'post_type'      => 'resource',
+				'posts_per_page' => 4, // TODO: Update to 6.
+				'paged'          => $paged,
+				's'              => $query,
+			)
+		);
+	} else {
+		$context['resources'] = Timber::get_posts(
+			array(
+				'post_type'      => 'resource',
+				'posts_per_page' => 4, // TODO: Update to 6.
+				'paged'          => $paged,
+				's'              => $query,
+				'meta_query'     => array(
+					'relation' => 'OR',
+					...array_map(
+						function( $intervention ) {
+							return array(
+								'key'     => 'interventions',
+								'value'   => '"' . $intervention . '"',
+								'compare' => 'LIKE',
+							);
+						},
+						$interventions
+					),
+				),
+			)
+		);
+	}
 
 	$interventions = Timber::get_posts(
 		array(
-			'post_type' => 'intervention',
+			'post_type'      => 'intervention',
 			'posts_per_page' => -1,
-			'orderby' => 'title',
-			'order' => 'ASC',
+			'orderby'        => 'title',
+			'order'          => 'ASC',
 		)
 	);
 
