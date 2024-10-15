4fa96e025c0747df9e04863dee0c4ab80a656afc
gvjacob <gvjacob@outlook.com>
2024-10-15 16:53:35 -0400

    Register featured content block

---
+++ themes/the-equity-fund/blocks/featured-content/featured-content.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * States block.
+ *
+ * @package TheEquityFund
+ * @param array $block The block settings and attributes.
+ */
+
+use TheEquityFund\Models\State;
+use Timber\Timber;
+
+$context = Timber::context();
+
+$context['headline'] = get_field( 'headline' );
+
+$provided_states_ids = get_field( 'states' );
+
+$states = Timber::get_posts(
+	array(
+		'post_type'      => State::POST_TYPE,
+		'post_status'    => 'publish',
+		'post__in'       => $provided_states_ids && count( $provided_states_ids ) > 0 ? $provided_states_ids : false,
+		'posts_per_page' => -1,
+		'orderby'        => 'post__in',
+	)
+);
+
+$context['states']              = $states;
+$context['show_grantees_count'] = get_field( 'show_grantees_count' );
+$context['cta']                 = get_field( 'cta' );
+
+Timber::render( basename( __DIR__ ) . '/states.twig', $context );
