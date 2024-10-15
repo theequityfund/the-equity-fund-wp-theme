c603b9da33cacbc5c5ebc84377edccbdab71ff6b
gvjacob <gvjacob@outlook.com>
2024-10-14 22:13:08 -0400

    Basic styling

--- themes/the-equity-fund/blocks/people/people.php
+++ themes/the-equity-fund/blocks/people/people.php
@@ -6,11 +6,24 @@
  * @param array $block The block settings and attributes.
  */
 
+use TheEquityFund\Models\Person;
 use Timber\Timber;
 
 $context = Timber::context();
 
 $context['headline']    = get_field( 'headline' );
-$context['people'] = get_field( 'people' );
+
+$people_ids = get_field( 'people' );
+
+$context['people'] = Timber::get_posts(
+	array(
+		'post_type'      => Person::POST_TYPE,
+		'post_status'    => 'publish',
+		'post__in'       => $people_ids,
+		'posts_per_page' => -1,
+		'orderby'        => 'post__in',
+	)
+);
+
 
 Timber::render( basename( __DIR__ ) . '/people.twig', $context );
