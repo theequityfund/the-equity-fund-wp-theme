3bfc0a2721ffd63ff9542a0421a18e5fa81a615f
gvjacob <gvjacob@outlook.com>
2024-10-24 14:17:49 -0400

    Query by issue and state

--- themes/the-equity-fund/blocks/grantees/grantees.php
+++ themes/the-equity-fund/blocks/grantees/grantees.php
@@ -15,15 +15,58 @@ $context['headline'] = get_field( 'headline' );
 
 $grantee_ids = get_field( 'grantees' );
 
-$context['grantees'] = Timber::get_posts(
-	array(
-		'post_type'      => Grantee::POST_TYPE,
-		'post_status'    => 'publish',
-		'post__in'       => $grantee_ids,
-		'posts_per_page' => -1,
-		'orderby'        => 'post__in',
-	)
-);
+$issue = get_field( 'issue' );
+$state = get_field( 'state' );
 
+if ( $grantee_ids && count( $grantee_ids ) ) {
+	$context['grantees'] = Timber::get_posts(
+		array(
+			'post_type'      => Grantee::POST_TYPE,
+			'post_status'    => 'publish',
+			'post__in'       => $grantee_ids,
+			'posts_per_page' => -1,
+			'orderby'        => 'post__in',
+		)
+	);
+} elseif ( $issue || $state ) {
+	$meta_query = array(
+		'relation' => 'AND',
+	);
+
+	if ( $issue ) {
+		$meta_query[] = array(
+			'key'     => 'issues',
+			'value'   => '"' . $issue . '"',
+			'compare' => 'LIKE',
+		);
+	}
+
+	if ( $state ) {
+		$meta_query[] = array(
+			'key'     => 'states',
+			'value'   => '"' . $state . '"',
+			'compare' => 'LIKE',
+		);
+	}
+
+	$context['grantees'] = Timber::get_posts(
+		array(
+			'post_type'      => Grantee::POST_TYPE,
+			'post_status'    => 'publish',
+			'posts_per_page' => -1,
+			'orderby'        => 'post_date',
+			'meta_query'     => $meta_query,
+		)
+	);
+} else {
+	$context['grantees'] = Timber::get_posts(
+		array(
+			'post_type'      => Grantee::POST_TYPE,
+			'post_status'    => 'publish',
+			'posts_per_page' => -1,
+			'orderby'        => 'post_date',
+		)
+	);
+}
 
 Timber::render( basename( __DIR__ ) . '/grantees.twig', $context );
