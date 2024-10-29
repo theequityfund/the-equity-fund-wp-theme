cf0667a9c6ebe32ffc1718d4b678b4d08ebb4c88
gvjacob <gvjacob@outlook.com>
2024-10-28 23:03:36 -0400

    Render cta and images

--- themes/the-equity-fund/blocks/statement/statement.php
+++ themes/the-equity-fund/blocks/statement/statement.php
@@ -13,6 +13,8 @@ $context = Timber::context();
 $context['headline']    = get_field( 'headline' );
 $context['statement']   = get_field( 'statement' );
 $context['description'] = get_field( 'description' );
+$context['images']      = get_field( 'images' );
+$context['cta']         = get_field( 'cta' );
 $context['variant']     = get_field( 'variant' );
 
 Timber::render( basename( __DIR__ ) . '/statement.twig', $context );
