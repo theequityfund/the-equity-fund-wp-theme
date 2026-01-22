d04243d084b1ec60d95b18362e784935502aa45d
volodymyrbabak1 <volodymyr.babak@upstatement.com>
2026-01-22 18:29:38 -0500

    EF-63 - update teh fields and template

--- web/wp-content/themes/the-equity-fund/blocks/cta/cta.php
+++ web/wp-content/themes/the-equity-fund/blocks/cta/cta.php
@@ -14,22 +14,17 @@ $context['headline']     = get_field( 'headline' );
 $context['introduction'] = get_field( 'introduction' );
 $context['palette']      = get_field( 'palette' );
 $context['cta_link']     = get_field( 'cta_link' );
-$raw_items               = get_field( 'cta_items' );
-$items                   = array();
+
+$raw_items = get_field( 'cta_items' );
+$items     = array();
 
 if ( ! empty( $raw_items ) && is_array( $raw_items ) ) {
-	foreach ( $raw_items as $item_id ) {
-		$item_post = get_post( $item_id );
-
-		if ( ! $item_post ) {
-			continue;
-		}
-
+	foreach ( $raw_items as $row ) {
 		$items[] = array(
-			'title'       => get_the_title( $item_post ),
-			'link'        => get_permalink( $item_post ),
-			'image'       => get_post_thumbnail_id( $item_post ),
-			'description' => get_field( 'description', $item_post->ID ) ? get_field( 'description', $item_post->ID ) : get_the_excerpt( $item_post ),
+			'title'        => $row['title'] ?? '',
+			'description'  => $row['description'] ?? '',
+			'image'        => $row['image'] ?? null,
+			'link'         => $row['link'] ?? null,
 		);
 	}
 }
